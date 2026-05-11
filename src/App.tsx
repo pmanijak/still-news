a5c25b94fba4f5c6b86f445016af4b71269098b4
Philip Manijak <130693+pmanijak@users.noreply.github.com>
2026-05-10 22:05:33 -0700

    Introduce Anthropic SDK
    Basic "Hello, Claude!" demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { Anthropic } from "@anthropic-ai/sdk/client.js";
+
+const client = new Anthropic({
+  // Put your Anthropic SDK key in a .env.local file,
+  // which vite picks up automatically.
+  apiKey: import.meta.env.VITE_ANTHROPIC_API_KEY,
+  dangerouslyAllowBrowser: true
+});
+
+const message = await client.messages.create({
+  max_tokens: 1024,
+  messages: [{ role: "user", content: "Hello, Claude" }],
+  model: "claude-haiku-4-5"
+});
+
 function App() {
+
+  const news = message.content
+    .filter(x => x.type === 'text')
+    .map(x => x.text)
+    .join('');
 
   return (
     <>
-      <div>still-news</div>
+      <div>{news}</div>
     </>
   )
 }
