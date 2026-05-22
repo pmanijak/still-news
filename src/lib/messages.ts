df102811af9e86886a6315e8039c0f2d8519b3e0
Philip Manijak <130693+pmanijak@users.noreply.github.com>
2026-05-21 20:19:10 -0700

    messages: clear test prompt

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -12,17 +12,7 @@ export function stream(settings: NewsSettings, signal?: AbortSignal) {
 
     const positivePercent = settings.positive;
     const positiveInstructions =
-        `When finding news, report ${positivePercent}% good news.
-         Don't indicate which news is good. If you can't find good 
-         news immediately on the first attempt, that is ok, just 
-         try again without telling me. You might need to search 
-         non-corporate news sources. Avoid news about large corporations
-         unless it is very good, and favor news stories that
-         affect a lot of people, not just simple stories about something
-         good that happened to one person. Include social progress,
-         and victories for working-class people and families. Avoid
-         the false pretense that capitalism is good. Highlight human stories,
-         though remember that humans are part of nature.`;
+        `When finding news, report ${positivePercent}% good news.`;
 
     const includeTopics = settings.topics
         .filter(x => x.selected)
