ab7fa94035bb8003ccae93b2d3de4a2ac14a8daf
Philip Manijak <130693+pmanijak@users.noreply.github.com>
2026-05-31 14:19:48 -0700

    useNews: add citations to the bottom

--- src/lib/useNews.ts
+++ src/lib/useNews.ts
@@ -1,23 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { stream } from "./messages";
 
 import type { NewsSettings } from '../NewsSettings';
+import type { CitationsWebSearchResultLocation } from "@anthropic-ai/sdk/resources.js";
+
+function getCitationMarkdownUrl(citation: CitationsWebSearchResultLocation) {
+  return `[${citation.title}](${citation.url})`;
+}
 
 export default function useNews(settings: NewsSettings) {
   const [news, setNews] = useState("");
+  const citationsRef = useRef<Record<string, CitationsWebSearchResultLocation>>({});
 
   useEffect(() => {
     const noNews = '';
     const ctrl = new AbortController();
     const res = stream(settings, ctrl.signal);
 
-    res.on("connect", () => setNews(noNews));
+    function guard <T extends unknown[]>(fn: (...args: T) => void) {
+      return (...args: T) => {
+        if (ctrl.signal.aborted)
+          return;
+        fn(...args);
+      };
+    }
 
-    res.on("text", (chunk) => {
-      if (ctrl.signal.aborted)
-        return;
+    res.on("connect", guard(() => {
+      setNews(noNews);
+      citationsRef.current = {};
+    }));
+
+    res.on("text", guard(chunk => {
       setNews((t) => t + chunk);
-    });
+    }));
+
+    res.on("citation", guard(citation => {
+      if (citation.type === 'web_search_result_location') {
+        citationsRef.current[citation.url] = citation;
+      }
+    }));
+
+    res.on("finalMessage", guard(() => {      
+      const sources = Object.values(citationsRef.current)
+        .map(x => " * " + getCitationMarkdownUrl(x))
+        .join("\n");
+
+      setNews(news => news + `\n\n### Sources\n\n${sources}`);
+    }));
 
     return () => ctrl.abort();
   }, [settings]);
